b96b05eb4d41fa9fda295710ec737fe982891cd6
Erik <eekihenriksson@gmail.com>
2024-09-14 22:11:58 +0300

    Update train_multiclass_2.py

--- train_multiclass_2.py
+++ train_multiclass_2.py
@@ -30,7 +30,7 @@ if not testing:
     )
 
     model = XLMRobertaForSequenceClassification.from_pretrained(
-        model_name, num_labels=5
+        model_name, num_labels=6
     )
     model.resize_token_embeddings(len(tokenizer))
 if create_dataset:
